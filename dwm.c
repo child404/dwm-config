da20078c6f43ee40e079e7e971a9138a5d5d83cd
child404 <vladrez1703@gmail.com>
2020-01-10 16:42:34 +0200

    center

--- dwm.c
+++ dwm.c
@@ -1049,7 +1049,6 @@ manage(Window w, XWindowAttributes *wa)
 		&& (c->x + (c->w / 2) < c->mon->wx + c->mon->ww)) ? bh : c->mon->my);
 	c->bw = borderpx;
 
-
 	wc.border_width = c->bw;
 	XConfigureWindow(dpy, w, CWBorderWidth, &wc);
 	XSetWindowBorder(dpy, w, scheme[SchemeNorm][ColBorder].pixel);
